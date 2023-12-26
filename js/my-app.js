3246e18e9afabd7566facde9c36f6b681fe38d4a
lore-pass <lorepass@hotmail.com>
2023-12-26 12:14:39 -0300

    actualizacion3

--- js/my-app.js
+++ js/my-app.js
@@ -25,11 +25,11 @@ var app = new Framework7({
       { path: '/reglas/',                url: 'reglas.html',   },
       { path: '/historialderespuestas/', url: 'historialderespuestas.html',   },
       { path: '/ajustes/',               url: 'ajustes.html',   },
-      { path: '/acercadelaaplicacion/',  url: 'acerca de la aplicación.html',   },
+      { path: '/acercadelaaplicacion/',  url: 'acercadelaaplicación.html',   },
       { path: '/chatbot/',               url: 'chatbot.html',   },
-      { path: '/funcioneschatbot/',      url: 'funcioneschatbot.js',   },
-      { path: '/producto/',              url: 'producto.js',   },
-      { path: '/notificaciones/',        url: 'notificaciones.js',   },
+      { path: '/funcioneschatbot/',      url: 'funcioneschatbot.html',   },
+      { path: '/producto/',              url: 'producto.html',   },
+      { path: '/notificaciones/',        url: 'notificaciones.html',   },
       
     ]
     // ... other parameters
@@ -223,20 +223,17 @@ function cargarDatosUsuarioLogueado() {
         }
     })
 }
-document.querySelectorAll('.icon-link').forEach(function(el) {
-    el.addEventListener('click', function() {
-      // Eliminar la clase 'active-icon' de todos los enlaces
-      document.querySelectorAll('.icon-link').forEach(function(link) {
-        link.classList.remove('active-icon');
-      });
-  
-      // Agregar la clase 'active-icon' a este enlace
-      this.classList.add('active-icon');
-  
-      // Opcional: cerrar el panel después de la selección
-      app.panel.close();
-    });
-  });
+// Usando Framework7 para seleccionar elementos y manejar eventos
+$$('.icon-link').on('click', function() {
+  // Eliminar la clase 'active-icon' de todos los enlaces
+  $$('.icon-link').removeClass('active-icon');
+
+  // Agregar la clase 'active-icon' a este enlace
+  $$(this).addClass('active-icon');
+
+  // Opcional: cerrar el panel después de la selección
+  app.panel.close();
+});
 
   //firebase//
   
@@ -246,7 +243,7 @@ var receivedMessagesRef = firebase.firestore().collection("mensajesRecibidos");
 //guarda las reglas definidas para el bot//
 function guardarConfiguracion() {
     var rolBot = document.getElementById('rol-bot').value;
-    var reglasMensaje = document.getElementById('reglas-mensaje').value;
+    var reglasMensaje = $$('reglas-mensaje').value;
   
     // Almacenar en Firestore
     colReglas.add({
@@ -266,20 +263,18 @@ function guardarConfiguracion() {
   
   
 
-document.querySelectorAll('.eliminar-regla').forEach(boton => {
-    boton.addEventListener('click', function() {
-      const idRegla = this.getAttribute('data-id');
-      // Lógica para eliminar la regla en Firebase
-      firebase.database().ref('ruta/a/las/reglas/' + idRegla).remove()
-      .then(() => {
+  $$('.eliminar-regla').on('click', function() {
+      const idRegla = $$(this).attr('data-id');
+       // Lógica para eliminar la regla en Firebase
+    firebase.firestore().collection('ruta/a/las/reglas').doc(idRegla).delete()
+    .then(() => {
         console.log('Regla eliminada');
         // Código adicional para actualizar la UI, si es necesario
-      })
-      .catch(error => {
+    })
+    .catch(error => {
         console.error('Error al eliminar la regla:', error);
-      });
     });
-  });
+});
   //notificaciones//
   colMensajesRecibidos.onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
@@ -293,7 +288,7 @@ document.querySelectorAll('.eliminar-regla').forEach(boton => {
     app.dialog.alert(`Nuevo mensaje: ${mensaje.texto}`);
   }
   function guardarMensaje() {
-    var mensajeTexto = document.getElementById('mensajeInput').value;
+    var mensajeTexto = $$('mensajeInput').value;
     if (!mensajeTexto) {
       alert("Por favor, escribe un mensaje.");
       return;
@@ -323,3 +318,121 @@ function cargarMensajes() {
 
 // Cargar mensajes al iniciar
 cargarMensajes()};
+//ajustes//
+$$('#configuracionBotForm').on('form:submit', function (e) {
+  e.preventDefault();
+  var formData = app.form.convertToData('#configuracionBotForm');
+  // Guardar datos en Firebase
+  firebase.firestore().collection('ajustes').doc('configuracionBot').set(formData)
+    .then(() => {
+      app.dialog.alert('Configuración guardada con éxito.');
+    })
+    .catch((error) => {
+      console.error("Error al guardar la configuración: ", error);
+    });
+});
+
+function cargarConfiguracionBot() {
+  firebase.firestore().collection('ajustes').doc('configuracionBot').get()
+    .then((doc) => {
+      if (doc.exists) {
+        app.form.fillFromData('#configuracionBotForm', doc.data());
+      }
+    });
+}
+
+// Llamada a la función al cargar la página
+cargarConfiguracionBot();
+
+// Función para guardar la configuración del bot
+function guardarConfiguracion() {
+  var rolBot = document.getElementById('rol-bot').value;
+  var reglasMensaje = document.getElementById('reglas-mensaje').value;
+
+
+  var docId = 'ID_DEL_DOCUMENTO'; // Asegúrate de tener un ID válido aquí
+
+  colReglas.doc(docId).set({
+      rolBot: rolBot,
+      reglasMensaje: reglasMensaje,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  })
+  .then(() => {
+      console.log("Configuración guardada con éxito");
+      // Aquí puedes agregar lo que sea necesario para confirmar al usuario que la operación fue exitosa
+  })
+  .catch(error => {
+      console.error("Error al guardar la configuración:", error);
+      // Manejar los errores aquí
+  });
+}
+
+// Función para cargar la configuración del bot al iniciar la página
+function cargarConfiguracionBot() {
+  var docId = 'ID_DEL_DOCUMENTO';
+
+  colReglas.doc(docId).get()
+  .then(doc => {
+      if (doc.exists) {
+          var datos = doc.data();
+          var rolBot = $$('#rol-bot').val();
+          var reglasMensaje = $$('#reglas-mensaje').val();
+          // Aquí puedes agregar más lógica si hay más campos para cargar
+      } else {
+          console.log("No se encontraron datos de configuración");
+          // Manejar el caso de que no haya datos
+      }
+  })
+  .catch(error => {
+      console.error("Error al cargar la configuración:", error);
+      // Manejar los errores aquí
+  })
+}
+
+// Llamar a cargarConfiguracionBot al iniciar la app o cuando sea necesario
+cargarConfiguracionBot();
+
+
+// Función para guardar la API de WhatsApp en Firestore
+function guardarApiWhatsapp(apiData) {
+  var db = firebase.firestore();
+  var configuracionRef = db.collection("configuracion").doc("apiWhatsApp");
+
+  configuracionRef.set(apiData)
+    .then(function() {
+      console.log("Detalles de la API de WhatsApp guardados con éxito!");
+    })
+    .catch(function(error) {
+      console.error("Error al guardar los detalles de la API:", error);
+    });
+}
+
+// Ejemplo de cómo usar la función
+// Suponiendo que 'apiData' es un objeto con los detalles de la API
+var apiData = {
+  token: "TU_TOKEN_DE_WHATSAPP",
+  // Otros detalles relevantes que necesites guardar
+};
+
+guardarApiWhatsapp(apiData);
+
+//Recuperación de la API de WhatsApp
+function obtenerApiWhatsapp() {
+  var db = firebase.firestore();
+  var configuracionRef = db.collection("configuracion").doc("apiWhatsApp");
+
+  configuracionRef.get()
+    .then(function(doc) {
+      if (doc.exists) {
+        var apiData = doc.data();
+        console.log("Datos de la API de WhatsApp:", apiData);
+        // Aquí puedes usar 'apiData' como necesites
+      } else {
+        console.log("No se encontraron detalles de la API de WhatsApp");
+      }
+    })
+    .catch(function(error) {
+      console.error("Error al obtener los detalles de la API:", error);
+    });
+  }
+
